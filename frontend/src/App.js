cc3cb2fc53119f0e2c9448560563575fe155b076
lensforkas <vi.p.oc.en.k.a1+016@gmail.com>
2024-10-02 16:32:23 +0200

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import GameBoard from './components/GameBoard';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <GameBoard />
+    </div>
+  );
+}
+
+export default App;
